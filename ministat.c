b851e479389aa88990fced5a5e9ba0458f7fe27f
sagunpandey123 <twiggymarilyn00@gmail.com>
2020-11-12 23:09:33 +0000

    Date

--- ministat.c
+++ ministat.c
@@ -5,7 +5,7 @@
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
  * ----------------------------------------------------------------------------
- *-------------------testing..changes 2222
+ *--------------Nov-12-2020----------
  */
 #include <sys/ioctl.h>
 
